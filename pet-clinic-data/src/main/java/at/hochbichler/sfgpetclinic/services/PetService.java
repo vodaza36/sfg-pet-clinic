fa207e1a263746ee4bc5f66ab11fadb3703da9f1
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-04 15:49:26 +0200

    add crud base interface an Closes #6.

--- pet-clinic-data/src/main/java/at/hochbichler/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/at/hochbichler/sfgpetclinic/services/PetService.java
@@ -2,10 +2,5 @@ package at.hochbichler.sfgpetclinic.services;
 
 import at.hochbichler.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }

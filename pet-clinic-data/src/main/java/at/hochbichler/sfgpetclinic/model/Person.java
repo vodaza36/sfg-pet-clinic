c9a3032fcaface651e37901d857dd0aef3f26e05
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-04 14:57:36 +0200

    Add Long ID. Closes #5.

--- pet-clinic-data/src/main/java/at/hochbichler/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/at/hochbichler/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package at.hochbichler.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/at/hochbichler/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/at/hochbichler/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package at.hochbichler.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {

6bd4eebb43149602af172cb31c99f55f6d6afcff
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-08 09:00:41 +0200

    adding owners index page and controller. Closes #10.

---
+++ pet-clinic-web/src/main/java/at/hochbichler/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package at.hochbichler.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ownerLists() {
+        return "owners/index";
+    }
+}
